2d504f3bffc2bb95031bcf790d727d349fb2450b
haituantuan <157444669+haituantuan@users.noreply.github.com>
2024-02-21 08:25:50 +0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to Welcome to my website
         </h1>
 
         <p className={styles.description}>
