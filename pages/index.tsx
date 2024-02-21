91c44594594af037c06a236c83f22c704fd6503c
haituantuan <157444669+haituantuan@users.noreply.github.com>
2024-02-21 08:30:38 +0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to Welcome to my website
+         Welcome to my website
         </h1>
 
 
